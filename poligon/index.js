5ad90df865254b7a6b3cf7609c40f00ba0d69e03
Dmitry <Jirnixu@yandex.ru>
2020-10-26 11:48:57 +0500

    'check it'

---
+++ poligon/index.js
@@ -0,0 +1,24 @@
+// function cs2() {
+//   let c = 0;
+//   return function () {
+//     c++;
+//     console.log(c);
+//     return c;
+//   };
+// }
+
+// let d = cs2();
+// d();
+// let sss = d;
+
+// console.log(sss);
+
+const getUser = async () => {
+  try {
+    const res = await fetch('https://vk.com');
+    const data = await res.json();
+  } catch (e) {
+    console.log(e);
+  }
+};
+getUser();
